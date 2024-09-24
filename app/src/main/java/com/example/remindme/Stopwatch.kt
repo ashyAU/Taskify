17d406f137d06d5b11a574b45d72583aacb6a128
Ash <ashy@tyty.cloud>
2024-09-24 16:42:35 +0800

    Implementing time support

--- app/src/main/java/com/example/remindme/Stopwatch.kt
+++ app/src/main/java/com/example/remindme/Stopwatch.kt
@@ -2,26 +2,25 @@ package com.example.remindme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import java.util.Calendar
 
 
@@ -31,6 +30,10 @@ fun StopwatchParent() {
     var isStarted by remember {
         mutableStateOf(false)
     }
+    var counter by remember {
+        mutableLongStateOf(0)
+    }
+
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -38,6 +41,8 @@ fun StopwatchParent() {
     ) {
         // todo, add timer!
         StopwatchButton(isStarted = isStarted, onStart = { isStarted = it })
+        StopWatchCounter(isStarted = isStarted, updateCount = { counter = it })
+        Text("$counter")
     }
 }
 
@@ -60,12 +65,22 @@ fun StopwatchButton(
 }
 
 @Composable
-fun StopwatchTime(isStarted: Boolean, onStart: (Boolean) -> Unit): String {
+fun StopWatchCounter(
+    isStarted: Boolean,
+    updateCount: (Long) -> Unit
+) {
 
-    val startedTime by remember {
-        mutableStateOf(Calendar.getInstance())
+    // need to store the value when timer is paused
+    var initialTime by remember {
+        mutableLongStateOf(0)
     }
-    val currentTime = Calendar.getInstance()
 
-    return ""
+    LaunchedEffect(isStarted) {
+        while (isStarted) {
+            delay(100)
+            updateCount(System.currentTimeMillis())
+        }
+    }
+
+
 }
