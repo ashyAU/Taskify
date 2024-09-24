445301ee806b0c9babeb69386dcb3cdd0e2c3dc9
Ash <ashy@tyty.cloud>
2024-09-24 21:17:39 +0800

    Got Timer working

--- app/src/main/java/com/example/remindme/MainActivity.kt
+++ app/src/main/java/com/example/remindme/MainActivity.kt
@@ -1,75 +1,50 @@
 package com.example.remindme
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.TimePicker
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Done
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionErrors
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.createBitmap
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
-import java.sql.Time
+
 import java.util.Calendar
-import java.util.Locale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -151,7 +126,7 @@ fun event(
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun ParentComposable() {
@@ -178,17 +153,24 @@ fun ParentComposable() {
                 navigationList.forEachIndexed { index, item ->
                     NavigationBarItem(
                         icon = {
-                            Icon(
-                                painterResource(id = item.iconUnfilled),
-                                contentDescription = item.label
-                            )
+                            if(selectedIndex == index)
+                            {
+                                Icon(
+                                    painterResource(id = item.iconFilled),
+                                    contentDescription = item.label
+                                )
+                            } else {
+                                Icon(
+                                    painterResource(id = item.iconUnfilled),
+                                    contentDescription = item.label
+                                )
+                            }
                         },
                         label = { Text(item.label) },
                         selected = selectedIndex == index,
                         onClick = { selectedIndex = index })
                 }
             }
-
         }
     )
     Box(modifier = Modifier.fillMaxSize()) {
--- app/src/main/java/com/example/remindme/Stopwatch.kt
+++ app/src/main/java/com/example/remindme/Stopwatch.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,7 +20,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
-import java.util.Calendar
+import java.util.Locale
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
 
 
 @Preview
@@ -42,6 +44,9 @@ fun StopwatchParent() {
         // todo, add timer!
         StopwatchButton(isStarted = isStarted, onStart = { isStarted = it })
         StopWatchCounter(isStarted = isStarted, updateCount = { counter = it })
+
+        // this is going to be the pretty print
+        TimeConversion(counter = counter, isStarted = isStarted)
         Text("$counter")
     }
 }
@@ -55,8 +60,8 @@ fun StopwatchButton(
         Icon(
             painter =
             when (isStarted) {
-                true -> painterResource(id = R.drawable.play_filled)
-                false -> painterResource(id = R.drawable.pause_filled)
+                true -> painterResource(id = R.drawable.pause_filled)
+                false -> painterResource(id = R.drawable.play_filled)
             },
             contentDescription = null,
             modifier = Modifier.fillMaxSize(0.5f)
@@ -71,16 +76,77 @@ fun StopWatchCounter(
 ) {
 
     // need to store the value when timer is paused
-    var initialTime by remember {
-        mutableLongStateOf(0)
+    val initialTime by remember {
+        mutableLongStateOf(System.currentTimeMillis())
     }
 
     LaunchedEffect(isStarted) {
         while (isStarted) {
             delay(100)
-            updateCount(System.currentTimeMillis())
+            updateCount(System.currentTimeMillis() - initialTime)
         }
     }
-
-
 }
+
+data class PrettyTime(
+    var millisecond: Long,
+    var second: Long,
+    var minute: Long,
+    var hour: Long
+)
+
+
+@Preview
+@Composable
+@OptIn(ExperimentalTime::class)
+fun TimeConversion(
+    isStarted: Boolean,
+    counter: Long
+) {
+    val prettyTime = remember { PrettyTime(0, 0, 0, 0) }
+
+    val duration = Duration
+    val totalMilliseconds = duration.convert(
+        value = counter.toDouble(),
+        sourceUnit = DurationUnit.MILLISECONDS,
+        targetUnit = DurationUnit.MILLISECONDS
+    ).toInt()
+
+    prettyTime.millisecond = (totalMilliseconds % 1000).toLong()
+    prettyTime.second = ((totalMilliseconds / 1000) % 60).toLong()
+    prettyTime.minute = ((totalMilliseconds / 60000) % 60).toLong()
+    prettyTime.hour = ((totalMilliseconds / 3600000).toLong())
+
+    LaunchedEffect(isStarted) {
+        while (isStarted) {
+            delay(16)
+            prettyTime.millisecond += 1
+
+            if (prettyTime.millisecond >= 1000) {
+                prettyTime.millisecond -= 1000
+                prettyTime.second++
+            }
+            if (prettyTime.second >= 60) {
+                prettyTime.second = 0
+                prettyTime.minute++
+            }
+            if (prettyTime.minute >= 60) {
+                prettyTime.minute = 0
+                prettyTime.hour++
+            }
+        }
+    }
+    val formattedTime = String.format(locale = Locale.getDefault(),
+        "%02d:%02d:%02d:%03d",
+        prettyTime.hour,
+        prettyTime.minute,
+        prettyTime.second,
+        prettyTime.millisecond
+    )
+    Text(text = formattedTime)
+}
+
+
+
+
+
